03476e3ddee7f6ab6ad220f770258a8edbb0a016
logicguy1 <logicguy.mailandcontact@gmail.com>
2022-10-04 10:10:23 +0200

    Update lesson 041022

--- Graphical User Interface/wxfb info man/gui.py
+++ Graphical User Interface/wxfb info man/gui.py
@@ -24,8 +24,11 @@ class MainFrame ( wx.Frame ):
 
 		bSizer4 = wx.BoxSizer( wx.VERTICAL )
 
-		self.m_button4 = wx.Button( self, wx.ID_ANY, u"MyButton", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_button4 = wx.Button( self, wx.ID_ANY, u"open dialog", wx.DefaultPosition, wx.DefaultSize, 0 )
 		bSizer4.Add( self.m_button4, 0, wx.ALL, 5 )
+
+		self.m_button6 = wx.Button( self, wx.ID_ANY, u"open window", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.m_button6, 0, wx.ALL, 5 )
 
 
 		self.SetSizer( bSizer4 )
@@ -35,6 +38,7 @@ class MainFrame ( wx.Frame ):
 
 		# Connect Events
 		self.m_button4.Bind( wx.EVT_BUTTON, self.open_dialog )
+		self.m_button6.Bind( wx.EVT_BUTTON, self.btn_open_window )
 
 	def __del__( self ):
 		pass
@@ -42,6 +46,9 @@ class MainFrame ( wx.Frame ):
 
 	# Virtual event handlers, override them in your derived class
 	def open_dialog( self, event ):
+		event.Skip()
+
+	def btn_open_window( self, event ):
 		event.Skip()
 
 
@@ -65,8 +72,8 @@ class MyDialog2 ( wx.Dialog ):
 
 		bSizer6.Add( self.m_staticText3, 0, wx.ALL, 5 )
 
-		self.m_textFname = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer6.Add( self.m_textFname, 0, wx.ALL, 5 )
+		self.m   = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer6.Add( self.m  , 0, wx.ALL, 5 )
 
 		self.m_staticText4 = wx.StaticText( self, wx.ID_ANY, u"Efternavn", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText4.Wrap( -1 )
@@ -104,5 +111,44 @@ class MyDialog2 ( wx.Dialog ):
 
 	def __del__( self ):
 		pass
+
+
+###########################################################################
+## Class MySecondFrame
+###########################################################################
+
+class MySecondFrame ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+		bSizer7 = wx.BoxSizer( wx.VERTICAL )
+
+		self.m_staticText5 = wx.StaticText( self, wx.ID_ANY, u"Joe mam", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText5.Wrap( -1 )
+
+		bSizer7.Add( self.m_staticText5, 0, wx.ALL, 5 )
+
+		self.m_button5 = wx.Button( self, wx.ID_ANY, u"Load value", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer7.Add( self.m_button5, 0, wx.ALL, 5 )
+
+
+		self.SetSizer( bSizer7 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.m_button5.Bind( wx.EVT_BUTTON, self.btn_load_val )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, override them in your derived class
+	def btn_load_val( self, event ):
+		event.Skip()
 
 
--- Graphical User Interface/wxfb info man/main.py
+++ Graphical User Interface/wxfb info man/main.py
@@ -1,21 +1,33 @@
 import wx
 import gui
 
-import crypt
+class MySecondFrame(gui.MySecondFrame):
+    def __init__(self, parent):
+        gui.MySecondFrame.__init__(self, parent)
+
+    def btn_load_val(self, event):
+        MySecondFrame.Hide(self)
 
 class mainFrame(gui.MainFrame): 
     """
     Class to manage the graphical user interface
     
     Attributes:
+        fnavn: First name
+        enavn: Last name
+        gender: Gender
+        birth: Birthday
+        MySecondFrame: The second frame in the program
 
     Methods: 
-
+        open_dialog: Open a dialog box
         quit: Exit the program
     """
 
     def __init__(self, parent):
         gui.MainFrame.__init__(self, parent)
+        self.MySecondFrame = MySecondFrame(self)
+
         self.fnavn = ""
         self.enavn = ""
         self.gender = ""
@@ -35,6 +47,10 @@ class mainFrame(gui.MainFrame):
             print("name:", self.fnavn, self.enavn)
             print("birth:", self.birth)
             print("gender:", self.gender)
+
+    def btn_open_window(self, event):
+        print("btn")
+        self.MySecondFrame.Show(True)
 
     def quit(self, event):
         exit()

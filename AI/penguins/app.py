a52bf7fe2e8b84c2b404e7311ac722f226765058
logicguy <logicguy.mailandcontact@gmail.com>
2023-08-31 09:49:49 +0200

    More AI stuff + flask, say whaaaaa

--- AI/penguins/app.py
+++ AI/penguins/app.py
@@ -8,6 +8,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn import tree
 
+import warnings
+warnings.warn = lambda *args, **kwargs: 1+1
+
 app = Flask(__name__)
 
 with open('model.pkl', 'rb') as f:
@@ -27,7 +30,12 @@ def submit():
     print(data)
 
     prediction = model.predict(data)[0]
-    prediction = ["Adelie Penguin (Pygoscelis adeliae)", "Gentoo penguin (Pygoscelis papua)", "Chinstrap penguin (Pygoscelis antarctica)"][int(prediction)]
+    prediction = [
+        "Adelie Penguin (Pygoscelis adeliae)", 
+        "Gentoo penguin (Pygoscelis papua)", 
+        "Chinstrap penguin (Pygoscelis antarctica)"
+    ][int(prediction)]
+
     print(prediction)
 
     return jsonify({"status":"success", "value": prediction})
